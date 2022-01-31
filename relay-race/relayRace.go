9ba078d5a9ad599ba4143b1537cb1e8d0669487c
karinamaria <karinamaria163@gmail.com>
2022-01-31 18:54:28 -0300

    feat: add relay race to only one team

--- relay-race/relayRace.go
+++ relay-race/relayRace.go
@@ -7,24 +7,23 @@ import (
 )
 
 const (
-	NUMBER_TEAMS int = 5
+	NUMBER_TEAMS   int = 1
 	NUMBER_RUNNERS int = 4
-	METERS int = 100
+	METERS         int = 100
 )
 
-func Runner(waitGroup *sync.WaitGroup, nameTeam string, baton chan int, arrivalOrder chan string) {
+func Runner(waitGroup *sync.WaitGroup, nameTeam string, baton chan int) {
 	runner := <-baton
 
 	fmt.Printf("[%s] - Corredor %d correndo com o bastão\n", nameTeam, runner)
 	nextRunner := runner + 1
 	if nextRunner <= NUMBER_RUNNERS {
-		go Runner(waitGroup, nameTeam, baton, arrivalOrder) //Puting the next runner in his position
+		go Runner(waitGroup, nameTeam, baton) //Puting the next runner in his position
 	}
 	time.Sleep(time.Millisecond * time.Duration(METERS)) // Running with the baton
 
 	if runner == NUMBER_RUNNERS {
 		fmt.Printf("[%s] - Finalizou a corrida! \n", nameTeam)
-		arrivalOrder <- nameTeam
 		waitGroup.Done()
 		return
 	}
@@ -34,27 +33,17 @@ func Runner(waitGroup *sync.WaitGroup, nameTeam string, baton chan int, arrivalO
 }
 
 func main() {
-	var racingLanes []chan int //Channel to pass baton between the runners of each team
-	for i := 0; i < NUMBER_TEAMS; i++ {
-		racingLanes = append(racingLanes, make(chan int))
-	}
+	racingLanes := make(chan int) //Channel to pass baton between the runners of each team
+
 	//The race will only end when all teams finish the full course
 	var waitGroup sync.WaitGroup
 	waitGroup.Add(NUMBER_TEAMS)
 
-	arrivalOrder := make(chan string, NUMBER_TEAMS) //Buffered Channel to store arrival order
-	for i := 0; i < NUMBER_TEAMS; i++ {
-		nameTeam := fmt.Sprint("Equipe ", (i + 1))
-		go Runner(&waitGroup, nameTeam, racingLanes[i], arrivalOrder) //Put the first runners of each team in his position
-	}
+	nameTeam := fmt.Sprint("Equipe ", NUMBER_TEAMS)
+	go Runner(&waitGroup, nameTeam, racingLanes) //Put the first runners of each team in his position
+	racingLanes <- 1                             //Send the baton to first runner of each lane and start the run
 
-	for i := 0; i < NUMBER_TEAMS; i++ {
-		racingLanes[i] <- 1 //Send the baton to first runner of each lane and start the run
-	}
 	waitGroup.Wait()
 
 	fmt.Println("\n =============\nCorrida finalizada")
-	for i := 0; i < NUMBER_TEAMS; i++ {
-		fmt.Printf("%dº Lugar: %s\n", (i + 1), <-arrivalOrder)
-	}
 }
